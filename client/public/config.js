ff91e2d6a6e465fb53e75eb72f3613755bf1915d
vishnuchethana <2410030245@klh.edu.in>
2026-06-30 03:39:49 +0530

    commit

---
+++ client/public/config.js
@@ -0,0 +1,3 @@
+// Runtime config for the frontend. Replace the URL below with your deployed API URL.
+// Example: window.__API_URL__ = 'https://my-api-service.onrender.com/api';
+window.__API_URL__ = 'https://<YOUR_SERVER_SERVICE>.onrender.com/api';
