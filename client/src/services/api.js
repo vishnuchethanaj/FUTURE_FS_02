fdcf2d9bd260ccb7405e546d737f92bc8263fd84
vishnuchethana <2410030245@klh.edu.in>
2026-06-30 03:13:14 +0530

    commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api','https://future-fs-02-mini-crm-frontend.onrender.com'
 });
 
 // Attach JWT to every request if present
